efa1b4d565cf71a0b83a98efae0fd71afd099ab2
Raphael Ezekiel <etiabasiezekiel@ymail.com>
2025-02-22 13:13:44 +0100

    update package imports

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:kamona_svg_to_font/svg_to_font.dart';
+import 'package:svg_to_font/svg_to_font.dart';
 
 void main(List<String> arguments) async {
   exit(await SvgToFontCommandRunner().run(arguments));
